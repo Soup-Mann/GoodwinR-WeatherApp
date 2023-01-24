edd6252745d1c8ef70bc0be5b437ef7934378b9b
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2023-01-24 15:39:07 -0800

    fetches

--- scripts.js/app.js
+++ scripts.js/app.js
@@ -1,5 +1,6 @@
 
 getData()
+getData2()
 
 
     
@@ -7,6 +8,14 @@ function getData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.7974&lon=-121.2161&appid=876472bfa888fce903a2ff2b5d460cdf&units=imperial').then(response => response.text()).then(data => {
        let cpuResponse = data
        console.log(cpuResponse);
+})
+
+}
+
+function getData2() {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=37.7974&lon=-121.2161&appid=876472bfa888fce903a2ff2b5d460cdf&units=imperial').then(response => response.text()).then(data => {
+       let cpuResponse2 = data
+       console.log(cpuResponse2);
 })
 
 }
